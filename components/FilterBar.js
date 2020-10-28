00a97fa7cd0c2614004e775d262100369158ea8b
ajgist <59585641+ajgist@users.noreply.github.com>
2020-10-28 15:07:20 -0500

    changed app.spec.js

--- components/FilterBar.js
+++ components/FilterBar.js
@@ -13,24 +13,30 @@ import data from "../qualifications.json";
 
 const categoryList = Object.values(data);
 
-function TextStyle(props) { //change the text color if being selected
-    const item = props.item;
-    const filterSettings = props.filterSettings;
-    let isEmpty = false;
-    Object.entries(filterSettings).map(([name, value]) => {
-        if(name == item.title && (value == null || value.length == 0)){
-            isEmpty = true;
-        }
-    } )
-    if(isEmpty || props.cleared){
-        return <Text>{item.title}</Text>;
-    }else{
-        return <Text style={styles.filterText}>{item.title}</Text>;
+function TextStyle(props) {
+  //change the text color if being selected
+  const item = props.item;
+  const filterSettings = props.filterSettings;
+  let isEmpty = false;
+  Object.entries(filterSettings).map(([name, value]) => {
+    if (name == item.title && (value == null || value.length == 0)) {
+      isEmpty = true;
     }
+  });
+  if (isEmpty || props.cleared) {
+    return <Text>{item.title}</Text>;
+  } else {
+    return <Text style={styles.filterText}>{item.title}</Text>;
   }
+}
 
-
-const FilterBar = ({ showModal, setModalData, filterSettings, setClearedSetting, isSelected}) => {
+const FilterBar = ({
+  showModal,
+  setModalData,
+  filterSettings,
+  setClearedSetting,
+  isSelected,
+}) => {
   const { colors } = useTheme();
   const [cleared, setCleared] = useState(false);
   const [shrinkSize, setShrinkSize] = useState("100%");
@@ -48,17 +54,28 @@ const FilterBar = ({ showModal, setModalData, filterSettings, setClearedSetting,
         data={categoryList}
         horizontal={true}
         renderItem={({ item }) => (
-          <Chip onPress={() => filterPress(item)} style={styles.chip}>
-            <TextStyle item={item}  filterSettings={filterSettings} cleared={cleared} />
+          <Chip
+            onPress={() => filterPress(item)}
+            style={styles.chip}
+            data-cy={item}
+          >
+            <TextStyle
+              item={item}
+              filterSettings={filterSettings}
+              cleared={cleared}
+            />
           </Chip>
         )}
         keyExtractor={(item, index) => index.toString()}
         showsHorizontalScrollIndicator={false}
-        style={[styles.container, {
+        style={[
+          styles.container,
+          {
             backgroundColor: colors.primary,
-            width: shrinksize, }]}
+            width: shrinksize,
+          },
+        ]}
       />
-
     </View>
   );
   return flatlist;
@@ -67,19 +84,18 @@ const FilterBar = ({ showModal, setModalData, filterSettings, setClearedSetting,
 const styles = StyleSheet.create({
   container: {
     paddingVertical: 4,
-    paddingHorizontal:4,
+    paddingHorizontal: 4,
     maxHeight: 40,
   },
   chip: {
     height: 30,
     margin: 1,
   },
-  filterText:{
+  filterText: {
     color: "#ebae34",
     textAlignVertical: "top",
     fontWeight: "bold",
-  }
-
+  },
 });
 
 export default FilterBar;
--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -2,4 +2,12 @@ describe("Test App", () => {
   it("launches", () => {
     cy.visit("/");
   });
+
+  it("opens with Filter Bar", () => {
+    cy.visit("/");
+    cy.get("[data-cy=numQual]").should(
+      "contain",
+      "45 student(s) matched your qualifications."
+    );
+  });
 });
