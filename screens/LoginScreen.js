af033823471816deff389b24d60e785cefdc5892
tkkwon1998 <tkkwon1998@gmail.com>
2020-11-01 16:46:03 -0600

    firebase authentication

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -8,42 +8,44 @@ const LoginScreen = ({ navigation, auth, setAuth, user, setUser }) => {
   const [loginError, setLoginError] = useState("");
 
   const onLogin = () => {
+    console.log("login entered");
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .catch((err) => setLoginError(err.message));
-
-    navigation.navigate("tabs");
+      .then(navigation.navigate("tabs"))
+      .catch(err => console.log(err.message));
   };
 
   const onSignUp = () => {
-    firebase.auth().createUserWithEmailAndPassword(email, password);
+    console.log("sign up entered");
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(err => setLoginError(err.message));
   };
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 40 }}>SCANDIDATE</Text>
+      <Text style={{ fontSize: 40 }}>Scandidate</Text>
+      <Text style={{ fontSize: 40 }}>Recruiter Login</Text>
       <TextInput
         value={email}
-        onChangeText={(email) => setEmail(email)}
+        onChangeText={email => setEmail(email)}
         placeholder={"Email"}
         style={styles.input}
       />
       <TextInput
         value={password}
-        onChangeText={(password) => setPassword(password)}
+        onChangeText={password => setPassword(password)}
         placeholder={"Password"}
         secureTextEntry={true}
         style={styles.input}
       />
       <View style={{ flexDirection: "row" }}>
-        <Button title={"Login"} style={styles.input} onPress={() => onLogin} />
-        <Button
-          title={"Sign Up"}
-          style={styles.input}
-          onPress={() => onSignUp}
-        />
+        <Button title={"Login"} style={styles.input} onPress={onLogin} />
+        <Button title={"Sign Up"} style={styles.input} onPress={onSignUp} />
       </View>
+      <Text>{loginError}</Text>
     </View>
   );
 };
@@ -53,7 +55,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#ecf0f1",
+    backgroundColor: "#ecf0f1"
   },
   input: {
     width: 200,
@@ -62,8 +64,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "black",
     marginHorizontal: 10,
-    marginBottom: 10,
-  },
+    marginBottom: 10
+  }
 });
 
 export default LoginScreen;
