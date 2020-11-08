3a31d61cec022942f8bbb80124a435f8072a461a
HesslerN <nicolehessler2021@u.northwestern.edu>
2020-11-07 19:30:02 -0600

    cypress test for adding a student to a folder from the student details page

--- cypress/integration/StudentDetailScreen.spec.js
+++ cypress/integration/StudentDetailScreen.spec.js
@@ -1,17 +1,23 @@
 describe('<StudentDetailScreen />', () => {
     it('(user logs in)', () => {
       cy.visit('/');
-      cy.get('input').first().type("admin@admin.com")
-      cy.get('input').eq(1).type("123321")
-      cy.get('div[role=button]').first().click()
+      cy.get('input').first().type("admin@admin.com");
+      cy.get('input').eq(1).type("123321");
+      cy.get('div[role=button]').first().click();
     });
 
     it("is accessible by clicking a student's candidate card", () => {
-        cy.get('div').contains("Taylor Kesicki").click()
+        cy.get('div').contains("Wayne Rooney").click();
     });
 
     it('diplays skills', () => {
-        cy.get('div').should('contain', 'video games')
+        cy.get('div').should('contain', 'CSS');
+    });
+
+    it('allows the student to be added to folders', () => {
+        cy.get("div").contains("Save").should("not.exist"); //modal not visible yet
+        cy.get('div').contains('Add to Folder').click();
+        cy.get("div").contains("Save"); //modal is now displayed
     });
 
 });
