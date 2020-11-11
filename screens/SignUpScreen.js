8265e9a75f1d076a28d259d075a1206463bcbd64
tkkwon1998 <tkkwon1998@gmail.com>
2020-11-11 15:15:20 -0600

    student and recruiter signup

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,7 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { Button, Image, Text, TextInput, View, StyleSheet } from "react-native";
+import { Menu, useTheme } from "react-native-paper";
 
 const SignUpScreen = ({ navigation }) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const openMenu = () => setMenuVisible(true);
+  const closeMenu = () => setMenuVisible(false);
+
+  const { colors } = useTheme();
+
+  const [role, setRole] = useState("Select your role");
+
+  const selectRole = (
+    <Text style={styles.roleSelect} onPress={openMenu}>
+      {role}
+    </Text>
+  );
+  const menu = (
+    <Menu
+      visible={menuVisible}
+      onDismiss={closeMenu}
+      anchor={selectRole}
+      contentStyle={{ backgroundColor: colors.background }}
+    >
+      <Menu.Item
+        onPress={() => {
+          closeMenu();
+          setRole("Student");
+        }}
+        title="Student"
+      />
+      <Menu.Item
+        onPress={() => {
+          closeMenu();
+          setRole("Recruiter");
+        }}
+        title="Recruiter"
+      />
+    </Menu>
+  );
+
   return (
     <View style={styles.container}>
       <Image
@@ -9,6 +47,10 @@ const SignUpScreen = ({ navigation }) => {
         style={{ height: 100, width: 100 }}
       />
       <Text>Scandidate</Text>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Text>Role:</Text>
+        {menu}
+      </View>
     </View>
   );
 };
@@ -18,7 +60,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#ecf0f1",
+    backgroundColor: "#ecf0f1"
   },
   input: {
     width: 200,
@@ -27,8 +69,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "black",
     marginHorizontal: 10,
-    marginBottom: 10,
+    marginBottom: 10
   },
+  roleSelect: {
+    backgroundColor: "white",
+    padding: 5,
+    borderBottomWidth: 2,
+    width: 125
+  }
 });
 
 export default SignUpScreen;
