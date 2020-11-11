c5bf48de8e949207c52529c34b09cedd342da5ce
zeyh <zeyanghuang42@gmail.com>
2020-11-11 15:36:53 -0600

    add more signup

--- App.js
+++ App.js
@@ -75,6 +75,7 @@ const App = () => {
             component={SignUpScreen}
             name="signup"
             options={{ title: "Sign Up" }}
+            initialParams={{ auth, setAuth, user, setUser }}
           />
           <Login.Screen
             component={tabs}
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,15 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { Button, Image, Text, TextInput, View, StyleSheet } from "react-native";
 import { Menu, useTheme } from "react-native-paper";
+import { firebase } from "../firebase";
+const db = firebase.database().ref("users");
 
-const SignUpScreen = ({ navigation }) => {
+const SignUpScreen = ({ navigation, auth, setAuth, user, setUser }) => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [signupError, setSignupError] = useState("");
+  const [role, setRole] = useState("Select your role");
+
   const openMenu = () => setMenuVisible(true);
   const closeMenu = () => setMenuVisible(false);
 
   const { colors } = useTheme();
 
-  const [role, setRole] = useState("Select your role");
+
+
+  async function onSignUp() {
+    setSignupError("");
+    if(role == "Select your role"){
+        setSignupError("Please select your role")
+        return
+    }
+    if(email == ""){
+        setSignupError("Please provide an email address")
+        return
+    }
+    if(password != confirmPassword){
+        setSignupError("Password does not match")
+        return
+    }
+    var errorCode = "success";
+    const signUpAction = () => {
+      if (errorCode == "success") {
+        user = firebase.auth().currentUser.uid;
+        // update the list of users
+        db.update({
+          [user]: {
+            Folders: { Favorites: [-1] },
+          },
+        });
+      }
+    };
+    if(role == "Student"){
+        //do something
+    }
+    if(role == "Recruiter"){
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch((err) => setSignupError(err.message))
+        .then(signUpAction);
+    }
+
+  }
 
   const selectRole = (
     <Text style={styles.roleSelect} onPress={openMenu}>
@@ -51,6 +98,28 @@ const SignUpScreen = ({ navigation }) => {
         <Text>Role:</Text>
         {menu}
       </View>
+      <TextInput
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+        placeholder={"Email"}
+        style={styles.input}
+      />
+      <TextInput
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        placeholder={"Password"}
+        secureTextEntry={true}
+        style={styles.input}
+      />
+      <TextInput
+        value={confirmPassword}
+        onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
+        placeholder={"Confirm Password"}
+        secureTextEntry={true}
+        style={styles.input}
+      />
+     <Button title={"Sign Up"} style={styles.input} onPress={onSignUp} />
+     <Text>{signupError}</Text> 
     </View>
   );
 };
